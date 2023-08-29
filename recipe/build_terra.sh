72ff60e8ee0e1399ed8efe29d4e3e2500e112691
Will Shanks <wshaos@posteo.net>
2023-08-29 18:35:54 -0400

    Fix cargo-bundle-licenses paths

--- recipe/build_terra.sh
+++ recipe/build_terra.sh
@@ -1,2 +1,2 @@
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-${PYTHON} -m pip install . -vv
+cd terra && cargo-bundle-licenses --format yaml --output ../THIRDPARTY.yml
+${PYTHON} -m pip install ./terra -vv
