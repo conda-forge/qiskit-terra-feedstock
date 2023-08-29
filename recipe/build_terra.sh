a2003457071df03d9c3964df95a4e3cfdf5dd166
Will Shanks <wshaos@posteo.net>
2023-08-29 19:39:52 -0400

    Fix directory in terra script

--- recipe/build_terra.sh
+++ recipe/build_terra.sh
@@ -1,2 +1,2 @@
-cd terra && cargo-bundle-licenses --format yaml --output ../THIRDPARTY.yml
+(cd terra && cargo-bundle-licenses --format yaml --output ../THIRDPARTY.yml)
 ${PYTHON} -m pip install ./terra -vv
